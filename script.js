0360dc174e0dde440f9f6064ffea411a7ff4f773
dss-neto <192475519+dss-neto@users.noreply.github.com>
2025-03-05 12:33:42 -0300

    Make buttons not erase the grid if they aren't the erase all button

--- script.js
+++ script.js
@@ -20,71 +20,86 @@ rightDiv.appendChild(stats);
 const changeGrid = document.createElement('button');
 changeGrid.style.cssText = 'box-sizing:border-box; text-align: center; height: 40px; width: 140px; margin-left: 20px; margin-top: 20px;';
 changeGrid.textContent = "Change the grid's row/column";
+changeGrid.setAttribute('id', 'changeGrid');
 buttonContainer.appendChild(changeGrid);
 
 const darkButton = document.createElement('button');
 darkButton.style.cssText = 'box-sizing:border-box; text-align: center; height: 40px; width: 140px; margin-left: 20px; margin-top: 20px;';
 darkButton.textContent = "Light to dark";
+darkButton.setAttribute('id', 'darkButton');
 buttonContainer.appendChild(darkButton);
 
 const randomButton = document.createElement('button');
 randomButton.style.cssText = 'box-sizing:border-box; text-align: center; height: 40px; width: 140px; margin-left: 20px; margin-top: 20px;';
 randomButton.textContent = "Random color mode";
+randomButton.setAttribute('id', 'randomButton');
 buttonContainer.appendChild(randomButton);
+
+const resetButton = document.createElement('button');
+resetButton.style.cssText = 'box-sizing:border-box; text-align: center; height: 40px; width: 140px; margin-left: 20px; margin-top: 20px;';
+resetButton.textContent = "Erase all";
+resetButton.setAttribute('id', 'resetButton');
+buttonContainer.appendChild(resetButton);
 
 let darkerToggle = 0;
 let randomToggle = 0;
 let darkIndex = 1;
+let darkCount = 0;
 let sizeGlobal = 16;
 createSketch(sizeGlobal);
 updateStats()
 
-darkButton.addEventListener('click', () => {
-    switch (darkerToggle) {
-        case 0:
-            darkerToggle = 1;
+buttonContainer.addEventListener('click', (e) => {
+    const target = e.target;
+
+    switch (target.id) {
+        case 'darkButton':
+            switch (darkerToggle) {
+                case 0:
+                    darkerToggle = 1;
+                    darkIndex = 0.1;
+                    darkCount = 10
+                    break;
+                case 1:
+                    darkerToggle = 0;
+                    darkIndex = 1;
+                    darkCount = 0
+                    break;
+            }
             break;
-        case 1:
-            darkerToggle = 0;
-            darkIndex = 1;
+        
+        case 'randomButton':
+            switch (randomToggle) {
+                case 0:
+                    randomToggle = 1;
+                    break;
+                case 1:
+                    randomToggle = 0;
+                    break;
+            }
             break;
+        
+        case 'changeGrid':
+            while (true) {
+                size = prompt("What's the size of the row and column?\nMax: 100", "");
+                if (parseInt(size) !== 'NaN' && parseInt(size) < 101) {
+                    break;
+                } else if (size === null) {
+                    return;
+                }
+            }
+            sizeGlobal = parseInt(size);
+            createSketch(sizeGlobal);
+            break;
+        
+        case 'resetButton':
+            createSketch(sizeGlobal);
     }
-
-    updateStats()
-    createSketch(sizeGlobal);
-});
-
-randomButton.addEventListener('click', () => {
-    switch (randomToggle) {
-        case 0:
-            randomToggle = 1;
-            break;
-        case 1:
-            randomToggle = 0;
-            break;
-    }
-
     updateStats();
-    createSketch(sizeGlobal);
-});
-
-changeGrid.addEventListener('click', () => {
-    while (true) {
-        size = prompt("What's the size of the row and column?\nMax: 100", "");
-        if (parseInt(size) !== 'NaN' && parseInt(size) < 101) {
-            break;
-        } else if (size === null) {
-            return;
-        }
-    }
-
-    sizeGlobal = parseInt(size);
-    updateStats()
-    createSketch(sizeGlobal);
 });
 
 function updateStats() {
-    stats.innerHTML = `Current grid: ${sizeGlobal} x ${sizeGlobal}<br>Light to dark: ${darkerToggle}<br>Random colors: ${randomToggle}    `;
+    stats.innerHTML = `Current grid: ${sizeGlobal} x ${sizeGlobal}<br>Light to dark: ${darkCount}<br>Random colors: ${randomToggle}    `;
 }
 
 function randomizeFrom0to255() {
@@ -97,14 +112,15 @@ function makeDarker(element) {
     element.style.opacity = `${darkIndex}`
     if (darkIndex < 1) {
         darkIndex += 0.1;
+        darkCount -= 1;
+        updateStats();
+    } else if (darkIndex >= 1) {
+        darkerToggle = 0;
+        updateStats();
     }
 }
 
 function createSketch(rowColumn) {
-
-    if (darkerToggle === 1) {
-        darkIndex = 0.1;
-    }
 
     // Remove all columns/rows before adding new ones
 
@@ -123,29 +139,20 @@ function createSketch(rowColumn) {
         col.style.flex = '0 0 calc(100%/var(--row))';
         col.setAttribute('class', 'col');
         container.appendChild(col);
-    }
-    
-    const colList = document.querySelectorAll('div.col');
-    switch (randomToggle) {
-        case 0:
-            colList.forEach(col => {
-                col.addEventListener('mouseover', () => {
-                    if (darkerToggle === 1) makeDarker(col);
+
+        col.addEventListener('mouseover', () => {
+            switch (randomToggle) {
+                case 0:
                     col.style.backgroundColor = 'black';
-                });
-            });
-            break;
-        
-        case 1:
-            colList.forEach(col => {
-                col.addEventListener('mouseover', () => {
-                    if (darkerToggle === 1) makeDarker(col);
+                    break;
+                case 1:
                     col.style.setProperty('--color1', randomizeFrom0to255());
                     col.style.setProperty('--color2', randomizeFrom0to255());
                     col.style.setProperty('--color3', randomizeFrom0to255());
                     col.style.backgroundColor = 'rgb(var(--color1), var(--color2), var(--color3))';
-                });
-            });
-            break;
+                    break;
+            }
+            makeDarker(col);
+        });
     }
 }
